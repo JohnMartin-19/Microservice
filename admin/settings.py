dde6842d9dc262d2e7db1361acd482f1640b752c
John <johnnkonge2020@gmail.com>
2024-04-22 14:00:27 +0300

    fixed bug

--- admin/settings.py
+++ admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(0*1o$903x=4+=rj7y*3h8s9o)2*uj%#dx5g*l$2p&fofa#cf0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0',' http://127.0.0.1:8000/']
 
 
 # Application definition
