a02e7d5892a823030d5a6cde93a20fb9a6ecb3b0
JohnMartin-19 <johnnkonge2020@gmail.com>
2024-04-23 17:07:56 +0300

    fixed bugs

--- admin/settings.py
+++ admin/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party
     'rest_framework',
-    'corsheaders',
+    #'corsheaders',
+    #my apps
     'products',
 ]
 
@@ -79,8 +81,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adminJ',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'db',
+        'PORT':'3306'
     }
 }
 
