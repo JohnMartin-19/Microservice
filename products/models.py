5bab018eb3a174ffd5e616df6450843f1deb95d9
JohnMartin-19 <johnnkonge2020@gmail.com>
2024-05-20 14:01:25 +0300

    Done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    likes = models.PositiveIntegerField()
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique = True)
